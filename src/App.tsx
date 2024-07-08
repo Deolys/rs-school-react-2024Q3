61435ee359beaecfdd51415f51b468f3a049ca81
Deolys <ed03aleks@mail.ru>
2024-07-08 22:25:25 +0300

    init: start react routing and tests task

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { Component, ReactNode } from 'react';
 import { Main } from './pages/main';
 
-export class App extends Component {
-  render(): ReactNode {
-    return <Main />;
-  }
-}
+const App = (): JSX.Element => {
+  return <Main />;
+};
 
 export default App;
