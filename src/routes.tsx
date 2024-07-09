b12fc8b007598e9a1848cf3a76c4f7eef56049da
Deolys <ed03aleks@mail.ru>
2024-07-09 15:26:51 +0300

    feat: add 404 page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import type { JSX } from 'react';
-import { Main } from './pages/main';
 import { Route, Routes } from 'react-router-dom';
+import { Main } from './pages/main';
+import { NotFoundPage } from './pages/not-found-page';
 
 export const PageRoutes = (): JSX.Element => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
