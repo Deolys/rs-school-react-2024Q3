6f2e9ed1791d50a20ddf04aabdf16ab1fd10d581
Deolys <ed03aleks@mail.ru>
2024-07-13 13:54:17 +0300

    feat: add fallback tests and update card style

--- jest.config.ts
+++ jest.config.ts
@@ -4,8 +4,8 @@ export default {
   collectCoverageFrom: [
     './src/**/*.ts?(x)',
     '!./src/main.tsx',
-    '!./src/app.tsx',
     '!./src/vite-env.d.ts',
+    '!./src/services/interfaces.ts',
   ],
   coverageDirectory: '<rootDir>/reports/coverage',
   coverageProvider: 'v8',
@@ -25,7 +25,7 @@ export default {
   roots: ['<rootDir>'],
   setupFiles: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/test/__mocks__/fileMock.js',
+    '\\.(gif|ttf|eot|svg|png|jpg)$': '<rootDir>/src/test/__mocks__/fileMock.js',
     '\\.(scss)$': 'identity-obj-proxy',
     '^@components/(.*)$': '<rootDir>/src/components/$1',
     '^@assets/(.*)$': '<rootDir>/src/assets/$1',
---
+++ src/__tests__/fallback-ui.test.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { FallbackUI } from '@components/fallback-ui';
+
+describe('Fallback UI', () => {
+  it('should render correctly', () => {
+    const { container } = render(<FallbackUI />);
+
+    expect(container).toBeInTheDocument();
+  });
+});
